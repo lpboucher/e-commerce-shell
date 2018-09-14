e658029451b79734364ec21514cd8b3d391c6f2a
LP Boucher <lp_boucher@hotmail.com>
2018-09-14 15:50:26 -0700

    created product card to be required in product list

---
+++ client/src/components/ProductCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const ProductCard = ({ title, image, price, description }) => {
+  return (
+    <Card>
+        <CardHeader
+          title={title}
+          subheader={price}
+        />
+        <CardMedia
+          image={image}
+          title={title}
+        />
+        <CardContent>
+          <Typography component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </Card>
+  );
+};
+
+export default ProductCard;
