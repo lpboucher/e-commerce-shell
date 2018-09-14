2757d3ecd59610bb938becfb825cb713fd399a73
LP Boucher <lp_boucher@hotmail.com>
2018-09-14 15:46:41 -0700

    require material ui and styled header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 
-class Header extends Component {
-    render() {
-        return (
-            <div>
+const Header = () => {
+    return(
+        <div>
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="title" color="inherit">
                 Header
-            </div>
-        );
-    }
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        </div>
+    )
 }
 
 export default Header;
