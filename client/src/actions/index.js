47d2bd44f26adacf6d68a587786222a643c096bd
LP Boucher <lp_boucher@hotmail.com>
2018-09-25 00:24:03 +0200

    separated product fetching into separate error and success actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,28 @@
 import axios from 'axios';
-import { FETCH_PRODUCT } from './types';
+import { PRODUCT_HAS_ERRORED, PRODUCTS_FETCH_DATA_SUCCESS } from './types';
+
+export const productHasErrored = (bool) => {
+    return {
+        type: PRODUCT_HAS_ERRORED,
+        hasErrored: bool,
+    };
+}
+
+export const productsFetchDataSuccess = (products) => {
+    return {
+        type: PRODUCTS_FETCH_DATA_SUCCESS,
+        products,
+    };
+}
 
 export const fetchProducts = () => async dispatch => {
-    const res = await axios.get('/api/product');
-    
-    dispatch({type: FETCH_PRODUCT, payload: res.data}); //maybe change to res.hello
+    try {
+        const res = await axios.get('/api/product');
+        console.log(res.data);
+        dispatch(productsFetchDataSuccess(res.data));
+    } catch(e) {
+        console.log(e);
+        dispatch(productHasErrored(true))
+    }
+    /*dispatch({type: FETCH_PRODUCT, payload: res.data});*/
 };
